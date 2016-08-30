3da84401a5bfbe42beb86415a59d6a09926afe34
Hans Kristian Henriksen <hans.kristian.henriksen@bekk.no>
2016-08-30 09:32:52 +0200

    Make interface norsk again

--- pong.js
+++ pong.js
@@ -157,8 +157,7 @@ Pong = {
       } else {
         document.getElementById("splash-end").classList.remove("hidden");
         var diff = this.scores[this.menu.winner] - this.scores[1 - this.menu.winner];
-        document.getElementById("end-info").innerHTML = "Player " + (this.menu.winner +1) + " wins the game with " + diff;
-        document.getElementById("end-info").innerHTML += diff > 1? " points" : " point";
+        document.getElementById("end-info").innerHTML = "Spiller  " + (this.menu.winner +1) + " vant med " + diff + " poeng";
       }
       
       this.playing = false;
@@ -279,9 +278,7 @@ Pong = {
   Menu: {
 
     initialize: function(pong) {
-      var winner = pong.images["images/winner.png"];
-      this.winner1 = { image: winner, x: (pong.width/2) - winner.width - pong.cfg.wallWidth, y: 6 * pong.cfg.wallWidth };
-      this.winner2 = { image: winner, x: (pong.width/2)                + pong.cfg.wallWidth, y: 6 * pong.cfg.wallWidth };
+      //Method no longer needed, but required to exist
     },
 
     declareWinner: function(playerNo) {
@@ -289,10 +286,7 @@ Pong = {
     },
 
     draw: function(ctx) {
-      if (this.winner == 0)
-        ctx.drawImage(this.winner1.image, this.winner1.x, this.winner1.y);
-      else if (this.winner == 1)
-        ctx.drawImage(this.winner2.image, this.winner2.x, this.winner2.y);
+      //Method no longer needed, but required to exist
     }
 
   },
